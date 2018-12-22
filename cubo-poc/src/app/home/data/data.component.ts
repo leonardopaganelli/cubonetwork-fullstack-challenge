b11cb812f62adc8b16d6f041dfc6e0c23d3c2094
Leonardo <leonardo.paganelli@iteris.com.br>
2018-12-22 12:48:33 -0200

    create of form and data component

---
+++ cubo-poc/src/app/home/data/data.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-data-form',
+    templateUrl: './data.component.html',
+    styleUrls: [
+        './data.component.scss'
+    ]
+})
+
+export class DataComponent { }
--- cubo-poc/src/app/home/home.module.ts
+++ cubo-poc/src/app/home/home.module.ts
@@ -1,15 +1,20 @@
-import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
-import { HomeRoutingModule } from './home.routing';
 import { RouterModule } from '@angular/router';
+
+import { DataComponent } from './data/data.component';
+import { FormComponent } from './form/form.component';
+import { HomeComponent } from './home.component';
+import { HomeRoutingModule } from './home.routing';
 
 @NgModule({
     declarations: [
+        DataComponent,
+        FormComponent,
         HomeComponent
     ],
     imports: [
-        RouterModule,
-        HomeRoutingModule
+        HomeRoutingModule,
+        RouterModule
     ]
 })
 
